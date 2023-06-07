c8b6282b57ce73c0d7d2c8e16287980017a606f5
Muhammad Usama <itxusama711@gmail.com>
2023-06-08 00:22:29 +0500

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,12 +1,4 @@
 
-from tkinter import Frame, Label, CENTER
-from random import randint
-import math
-import time
-import numpy as np
-from numba import jit
-from random import randint, seed
-
 
 UP, DOWN, LEFT, RIGHT = range(4)
 
