d43f20219301622c0cc4e31877234116a07cbaf4
Muhammad Usama <itxusama711@gmail.com>
2023-06-08 00:23:57 +0500

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -1,28 +1,4 @@
 
-                cell.grid(row=i, column=j, padx=GRID_PADDING, pady=GRID_PADDING)
-                # font = Font(size=FONT_SIZE, family=FONT_FAMILY, weight=FONT_WEIGHT)
-                t = Label(master=cell, text="", bg=BACKGROUND_COLOR_CELL_EMPTY, justify=CENTER, font=FONT, width=4, height=2)
-                t.grid()
-                grid_row.append(t)
-
-            self.grid_cells.append(grid_row)
-
-    def gen(self):
-        return randint(0, GRID_LEN - 1)
-
-    def init_matrix(self):
-        self.board = GameBoard()
-        self.add_random_tile()
-        self.add_random_tile()
-
-    def update_grid_cells(self):
-        for i in range(GRID_LEN):
-            for j in range(GRID_LEN):
-                new_number = int(self.board.grid[i][j])
-                if new_number == 0:
-                    self.grid_cells[i][j].configure(text="", bg=BACKGROUND_COLOR_CELL_EMPTY)
-                else:
-                    n = new_number
                     if new_number > 2048:
                         c = 2048
                     else:
